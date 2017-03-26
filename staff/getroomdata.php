678a8a09b0cd61ccab702b0ebeccfda88837628f
harshitbudhraja <harshitbudhraja1301@gmail.com>
2017-03-26 11:13:01 +0530

    Added aditional API functionalities

--- staff/getroomdata.php
+++ staff/getroomdata.php
@@ -2,5 +2,13 @@
 	error_reporting(0);
 	$con = mysqli_connect("localhost","root","");
 	mysqli_select_db($con,"sudorooms");
-	$sql = "select RoomNum,isFree,servicereq,isOpen from room";
-	mysqli_query($con,$sql);
+	$sql = "select * from room";
+	$result=mysqli_query($con,$sql);
+	$rows=array();
+	while($r = mysqli_fetch_assoc($result))
+	{
+		$rows[]=$r;
+	}
+	echo json_encode($rows);
+	mysqli_close($con);
+?>
