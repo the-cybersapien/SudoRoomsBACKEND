5b273a10ab266dc5ddacf6bb916ef664fd1ff12e
Aditya Aggarwal <aditya@cybersapien.xyz>
2017-03-26 09:32:19 +0530

    Added Exotel SMS API

--- reception-desk/registeruser.php
+++ reception-desk/registeruser.php
@@ -23,6 +23,34 @@ function generateAccessKey($length = 10) {
 					$sql = "UPDATE customer SET AccessKey = '".$access_key."' WHERE RoomAllotted = '".$roomchoice."'";
 					mysqli_query($con,$sql);
 					mysqli_close($con);
+
+          $post_data = array(
+            'From' => '09243422233',
+            'To' => '$mobile',
+            'Body' => 'Hello, your access key is: $access_key');
+
+          $exotel_sid = "cybersapien";
+          $exotel_token = "0b83f4803c68e21290f19eb534d9b427937db916";
+
+          $url = $url = "https://".$exotel_sid.":".$exotel_token."@twilix.exotel.in/v1/Accounts/".$exotel_sid."/Sms/send";
+
+          $ch = curl_init();
+          curl_setopt($ch, CULROPT_VERBOSE, 1);
+
+          curl_setopt($ch, CURLOPT_URL, $url);
+          curl_setopt($ch, CURLOPT_POST, 1);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+          curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+          curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+
+          $http_result = curl_exec($ch);
+          $error = curl_error($ch);
+          $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+          curl_close($ch);
+          echo "Response = ".print_r($http_result);
+
 					header("Location: /sudorooms/reception-desk/");
 					die();
 ?>
--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@
         function httpGetAsync(theUrl, callback) {
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.onreadystatechange = function () {
-                if (xmlHttp.status == 200){
+                if (xmlHttp.status == 200) {
                     callback(xmlHttp.responseText);
                 }
                 else
@@ -27,83 +27,48 @@
 
 </head>
 <body>
-    <div class="mdl-js-layout mdl-layout mdl-layout--fixed-header mdl-layout--no-drawer-button">
-        <header class="mdl-layout__header">
-            <div class="mdl-layout__header-row">
-                <span class="mdl-layout-title">Sudo Rooms Demo</span>
-                <div class="mdl-layout-spacer"></div>
-            </div>
-        </header>
+<div class="mdl-js-layout mdl-layout mdl-layout--fixed-header mdl-layout--no-drawer-button">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Sudo Rooms Demo</span>
+            <div class="mdl-layout-spacer"></div>
+        </div>
+    </header>
 
-        <p id="s1"></p>
-        <main class="mdl-layout__content" id="content-show">
+    <p id="s1"></p>
+    <main class="mdl-layout__content" id="content-show">
 
 
-
-                <?php
-                    for ($a = 0; $a < 4; $a++){
-                        echo "<div class=\"mdl-grid\">";
-                        for($i = 0; $i < 10; $i++){
-                            $guest_name = "Name $i";
-                            echo "<div class=\"room-card room-card-unlocked mdl-cell mdl-cell--1-col mdl-card mdl-shadow--2dp\">";
-                            echo "<div class=\"mdl-card__title mdl-card--expand\">";
-                            echo "<h4 id=\"room_number\">";
-                            echo "Room Number $i";
-                            echo "</h4>";
-                            echo "</div>";
-                            echo "<div class=\"mdl-card__actions mdl-card&#45;&#45;border mdl-card__title-text\">";
-                            echo "<span class=\"room-card__name\" id=\"guest_name\">$guest_name</span>";
-                            echo "</div>";
-                            echo "<div class=\"mdl-layout-spacer\"></div>";
-                            echo "</div>";
-                        }
-                        echo "</div>";
-                    }
-                ?>
-
-                <!--<div class="room-card room-card-locked mdl-card mdl-shadow&#45;&#45;2dp">-->
-                    <!--<div class="mdl-card__title mdl-card&#45;&#45;expand">-->
-                        <!--<h4 id="room_number">-->
-                            <!--Room Number 302-->
-                        <!--</h4>-->
-                    <!--</div>-->
-                    <!--<div class="mdl-card__actions mdl-card&#45;&#45;border mdl-card__title-text">-->
-                        <!--<span class="room-card__name" id="guest_name">Aditya Aggarwal</span>-->
-                    <!--</div>-->
-                    <!--<div class="mdl-layout-spacer"></div>-->
-                <!--</div>-->
-
-            </div>
-
-        </main>
-    </div>
-
-    
-    <script>
-        var parent = $(".mdl-grid")[0];
-        for (var i =0; i < 40; i++){
-            
-            var frame = $("<div>", {"class": "room-card room-card-locked mdl-card mdl-shadow--2dp"});
-            var title = $("<div>", {"class": "mdl-card__title mdl-card--expand"});
-            var head = $("<h5>", {"id": "room_number"});
-            head.text(i.toString());
-            var sub = $("<div>", {"class": "mdl-card__actions mdl-card--border mdl-card__title-text"});
-            var name = $("<span>", {"class": "room-card__name", "id": "guest_name"});
-            sub.append(html(name));
-            console.log(html(name));
-            title.appendChild(head);
-            frame.appendChild(title);
-            parent.appendChild(frame);
+        <?php
+        for ($a = 0; $a < 4; $a++) {
+            echo "<div class=\"mdl-grid\">";
+            for ($i = 0; $i < 10; $i++) {
+                $guest_name = "Name $i";
+                echo "<div class=\"room-card room-card-unlocked mdl-cell mdl-cell--1-col mdl-card mdl-shadow--2dp\">";
+                echo "<div class=\"mdl-card__title mdl-card--expand\">";
+                echo "<h4 id=\"room_number\">";
+                echo "Room Number $i";
+                echo "</h4>";
+                echo "</div>";
+                echo "<div class=\"mdl-card__actions mdl-card--border mdl-card__title-text\">";
+                echo "<span class=\"room-card__name\" id=\"guest_name\">$guest_name</span>";
+                echo "</div>";
+                echo "<div class=\"mdl-layout-spacer\"></div>";
+                echo "</div>";
+            }
+            echo "</div>";
         }
-        
-    </script>
+        ?>
+    </main>
+</div>
 
-    <script>
-        function call(txt) {
-            document.getElementById("s1").innerHTML = txt;
-        }
-        httpGetAsync("http://192.168.2.214/sudorooms/customer/request_open.php?room=15&action=OPEN", call);
-    </script>
+
+<script>
+    function call(txt) {
+        document.getElementById("s1").innerHTML = txt;
+    }
+    httpGetAsync("http://192.168.2.214/sudorooms/customer/request_open.php?room=15&action=OPEN", call);
+</script>
 
 </body>
 </html>
